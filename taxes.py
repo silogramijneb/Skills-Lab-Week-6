44f7338c749bc3e2aed90200c3bd7cd6d3f17903
silogramijneb <71283314+silogramijneb@users.noreply.github.com>
2020-09-23 16:08:32 -0400

    Update taxes.py

--- taxes.py
+++ taxes.py
@@ -6,3 +6,5 @@ state_tax = taxable * .0307
 local_tax = taxable * .01
 property_tax = taxable * .062
 federal_tax = taxable * .1
+take_home = taxable - state_tax - local_tax - property_tax - federal_tax
+print(user_name, "has a take home income of $", take_home)
