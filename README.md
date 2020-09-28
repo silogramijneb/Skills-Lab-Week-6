f88ba588013c759c1368952c050d9a81e39b3595
BrianLucas26 <btl26@pitt.edu>
2020-09-28 13:46:04 -0400

    Updated README file to explain the program and added comments to the taxes program.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Skills-Lab-Week-6
 
-Hi Benji, this is a test sentence.
+Our program calculates taxes for the user, using income and their retirement plan. 
+
+You first enter your name, followed by your income and your retirement plan, all seperated by one space like (Brian 34000 3200)
+
+The program will return the user name and amount of money they take home after taxes.
--- taxes.py
+++ taxes.py
@@ -1,8 +1,17 @@
+#This asks the user for their name
 user_name = input(prompt = "What is your name?")
+#Asks user for income
 income = input(prompt = "What is your income?")
+#asks user for retirement plan
 retire = input(prompt = "What is your 401k?")
+#next 5 lines calculate the taxes
 taxable = income - retire
 state_tax = taxable * .0307
 local_tax = taxable * .01
 property_tax = taxable * .062
 federal_tax = taxable * .1
+#calculates take home value by subtracting taxes
+take_home = taxable - state_tax - local_tax - property_tax - federal_tax
+#prints takehome value
+print(user_name, "has a take home income of $", take_home)
+
